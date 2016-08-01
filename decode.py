6d5948cde23cc8ab5c6dace86a1c266f1154d219
daokun <daokunjiang@gmail.com>
2016-08-01 12:07:58 +0800

    Successfully decode from html, something wrong with selenium

--- decode.py
+++ decode.py
@@ -1,4 +1,42 @@
 from bs4 import BeautifulSoup as bs
 
 with open('xue.txt', 'r', encoding='utf-8') as f:
-    doc = bs(f.read())
+    doc = bs(f.read(), "lxml")
+    x = doc.find_all("tr", class_="gsc_a_tr")
+    len_x = len(x)
+    paper_name = []
+    paper_author = []
+    paper_magazine = []
+    paper_ref = []
+    paper_pubyear = []
+    for i in range(0, len_x):
+        paper_name_raw = x[i].find_all(class_="gsc_a_at")
+        paper_name.append(paper_name_raw[0].string)
+        paper_info_raw = x[i].find_all(class_="gs_gray")
+        paper_author.append(paper_info_raw[0].string)
+        magazine_raw = paper_info_raw[1].contents[0]
+        paper_magazine.append(magazine_raw.string)
+        paper_ref_raw = x[i].find_all(class_="gsc_a_c")
+        paper_ref_a = paper_ref_raw[0].contents[0].string
+        if paper_ref_a == '\xa0':
+            paper_ref.append('0')
+        else:
+            paper_ref.append(paper_ref_a)
+        paper_pubyear_raw = x[i].find_all(class_="gsc_a_y")
+        paper_pubyear.append(paper_pubyear_raw[0].string)
+    print(paper_name)
+    print(paper_author)
+    print(paper_magazine)
+    print(paper_ref)
+    print(paper_pubyear)
+
+    ref_year_raw = doc.find_all(id="gsc_g_x")[0]
+    ref_year = []
+    for year in ref_year_raw.children:
+        ref_year.append(year.string)
+
+    ref_num_raw = doc.find_all(id="gsc_g_bars")[0]
+    ref_num = []
+    for num in ref_num_raw.children:
+        ref_num.append(num.string)
+
--- main.py
+++ main.py
@@ -4,17 +4,31 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get('https://scholar.google.com/citations?hl=zh-CN&user=gM5i-iYAAAAJ&view_op=list_works&sortby=pubdate')
+#driver.find_element_by_id('gsc_bpf_more').click()
+driver.implicitly_wait(10)
 f = open('xue.txt', 'w', encoding='utf-8')
-results = ''
-re_len_before = 0
-while 1:
-    inputBtn = driver.find_element_by_id('gsc_bpf_more').click()
-    results = driver.page_source
-    re_len = len(results)
-    if (re_len_before != 0) & (re_len == re_len_before):
-        break
-    re_len_before = re_len
+results = driver.page_source
 
+# re_len_before = 0
+# count = 0
+# # while 1:
+# #     # results = driver.page_source
+# #     # re_len = len(results)
+# #     # print(re_len)
+# #     # if (re_len_before != 0) & (re_len == re_len_before):
+# #     #     break
+# #     # re_len_before = re_len
+# #     count += 1
+# #     try:
+# #         inputBtn = driver.find_element_by_id('gsc_bpf_more').click()
+# #         driver.implicitly_wait(3)
+# #     except EnvironmentError:
+# #         break
+#
+# print(count)
+#
+# driver.implicitly_wait(10)
+# results = driver.page_source
 f.write(results)
 
 # inputBtn.send_keys('bing xie')
